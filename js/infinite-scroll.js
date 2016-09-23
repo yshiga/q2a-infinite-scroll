e9be1181233c0e09d997e142890c52a7c542aa25
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-09-23 15:40:19 +0900

    Add css for ias

--- js/infinite-scroll.js
+++ js/infinite-scroll.js
@@ -5,13 +5,13 @@ $(function(){
 			,item: ".qa-q-list-item"
 			,pagination: ".qa-page-links-list"
 			,next: ".qa-page-next"
-			,noneleft: '<div class="ias_noneleft">No more items.</div>'
 			,delay: 600
 			,scrollContainer: $(window)
 		});
     ias.extension(new IASSpinnerExtension());
     ias.extension(new IASTriggerExtension({
         text: "続きを読む",
+        textPrev: "前を読む",
         offset: 100,
     }));ias.extension(new IASNoneLeftExtension({
       html: '<div class="ias_noneleft">最後の記事です</div>', // optionally
