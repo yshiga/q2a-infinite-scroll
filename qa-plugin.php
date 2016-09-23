266ae939f1402f2c83d9a21cf7825263045d5d29
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-09-23 12:02:17 +0900

    Add admin option

--- qa-plugin.php
+++ qa-plugin.php
@@ -9,7 +9,7 @@
     Plugin Author URI: https://38qa.net/
     Plugin License: GPLv2
     Plugin Minimum Question2Answer Version: 1.7
-    Plugin Update Check URI: https://github.com/yshiga/q2a-infinite-scroll
+    Plugin Update Check URI:
 */
 
 if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
@@ -17,7 +17,8 @@ if (!defined('QA_VERSION')) { // don't allow this page to be requested directly 
     exit;
 }
 
-qa_register_plugin_phrases('qa-infinite-scroll-lang-*.php', 'infinite-scroll');
+qa_register_plugin_phrases('qa-infinite-scroll-lang-*.php', 'infinite_scroll');
+qa_register_plugin_module('module', 'qa-infinite-scroll.php', 'qa_infinite_scroll', 'Infinite Scroll');
 
 /*
     Omit PHP closing tag to help avoid accidental output
